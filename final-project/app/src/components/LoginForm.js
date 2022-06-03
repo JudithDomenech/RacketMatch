b2ed469ec4625d06d4408a06a37b383734fc1bbd
Judith Domènech Pujadas <97442199+JudithDomenech@users.noreply.github.com>
2022-06-03 16:54:21 +0200

    Add feedback in Login

--- final-project/app/src/components/LoginForm.js
+++ final-project/app/src/components/LoginForm.js
@@ -1,8 +1,13 @@
 import './LoginForm.sass'
+import React, { useState } from 'react'
 import { authenticateUser } from '../logic'
 import { Button, Link, Input } from '.'
 
 export function LoginForm({ onLoggedIn, onRegister }) {
+
+    const [emailFeedback, setEmailFeedback] = useState()
+    const [passwordFeedback, setPasswordFeedback] = useState()
+    const [feedback, setFeedback] = useState()
 
     const login = async event => {
         try {
@@ -10,9 +15,18 @@ export function LoginForm({ onLoggedIn, onRegister }) {
 
             const token = await authenticateUser(email, password)
             sessionStorage.token = token
-            
+
             onLoggedIn()
-            
+            setFeedback()
+
+            if (message.includes('email')) {
+                setPasswordFeedback()
+                setEmailFeedback(message)
+            } else if (message.includes('password')) {
+                setEmailFeedback()
+                setPasswordFeedback(message)
+            }
+
         } catch (error) {
             alert(error.message)
         }
@@ -23,11 +37,16 @@ export function LoginForm({ onLoggedIn, onRegister }) {
         login(event)
     }
 
+    const clearPasswordFeedback = () => setPasswordFeedback()
+
+    const clearEmailFeedback = () => setEmailFeedback()
+
     return <form className='login__wrapper' onSubmit={onSubmit}>
-            <Input className='login__input'type='email' name='email' placeholder='Email' required={true} />
-            <Password className='login__input' type='password' feedback={passwordFeedback} onFocus={clearPasswordFeedback} name='password' placeholder='Contraseña' required={true} />
-            <Button className='login__button'type='submit'> Login </Button>
-            <Link href="" onClick={onRegister}> Registrarse</Link>     
+        <Input className='login__input' type='email' feedback={emailFeedback} onFocus={clearEmailFeedback} name='email' placeholder='Email' required={true} />
+        <Password className='login__input' type='password' feedback={passwordFeedback} onFocus={clearPasswordFeedback} name='password' placeholder='Contraseña' required={true} />
+        <Button className='login__button' type='submit'> Login </Button>
+        {feedback && <Feedback message={feedback} level="error" />}
+        <Link href="" onClick={onRegister}> Registrarse</Link>
     </form>
 }
 
