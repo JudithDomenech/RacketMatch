7d6d617c01599cc05776f72c4ca0228dda41ca0a
Judith Domènech Pujadas <97442199+JudithDomenech@users.noreply.github.com>
2022-06-03 16:47:26 +0200

    add Feedback

--- final-project/app/src/components/Feedback.js
+++ final-project/app/src/components/Feedback.js
@@ -1,6 +1,6 @@
-/* import './Feedback.sass'
+import './Feedback.sass'
 
 export function Feedback({ message, level = 'info', version = 'default' }) {
     return <span className={`feedback feedback--${level} feedback--${version}`}>{message}</span>
 }
- */
+ 
